f6b8df1df5e1ce33c5398d95df86996cfed40db7
KhomyakovDaniil <daniil.h@appomart.com>
2024-04-18 15:07:25 +0300

    Cleaning Removed leftover code

--- PhotoEditorDemo/Extensions/UIView.swift
+++ PhotoEditorDemo/Extensions/UIView.swift
@@ -9,12 +9,6 @@ import Foundation
 import UIKit
 
 extension UIView {
-//    var asImg: UIImage? {
-//        let renderer = UIGraphicsImageRenderer(bounds: bounds)
-//        return renderer.image { rendererContext in
-//            layer.render(in: rendererContext.cgContext)
-//        }
-//    }
     
     func snapshotImage() -> UIImage {
         UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, 0.0)
